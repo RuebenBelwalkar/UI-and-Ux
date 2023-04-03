a1643c3f00637a949219e55711ce496bd211b735
RuebenBelwalkar <ruebenbelwalkar10@gmail.com>
2023-04-03 15:18:05 +0530

    autowired and qualifier

--- springDemo/springdemo/src/main/java/com/ani/spring/excel/XLManager.java
+++ springDemo/springdemo/src/main/java/com/ani/spring/excel/XLManager.java
@@ -1,17 +1,31 @@
 package com.ani.spring.excel;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
+import com.ani.spring.util.FileUtil;
 
 @Component
 public class XLManager {
     @Autowired
     private XLReader reader;
+
     @Autowired
     private XLFormatter formatter;
+
+    @Autowired
+    private FileUtil util;
+    
+    @Autowired
+    @Qualifier("utilData")
+    private FileUtil utilAbc;
+    
 
     public void configXL() {
         reader.setReadLimit(10);
         formatter.setFont("abc");
+        util.fileSize();
+        utilAbc.fileSize();
     }
 }
