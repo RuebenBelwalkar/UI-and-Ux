4ac38635b3b74dde0c7c88dc2effb3e37a73df5f
RuebenBelwalkar <ruebenbelwalkar10@gmail.com>
2023-04-03 12:22:53 +0530

    done

---
+++ springDemo/springdemo/src/main/java/com/ani/spring/App.java
@@ -0,0 +1,49 @@
+package com.ani.spring;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.ani.spring.config.AppConfig;
+import com.ani.spring.util.FileChecker;
+import com.ani.spring.util.FileUtil;
+
+
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        public static void normal() {
+            FileUtil util = new FileUtil();
+    
+            FileChecker checker = new FileChecker(util);
+            checker.checkFileValidity();
+        }
+    
+        public static void withSpring() {
+            ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+    
+            FileChecker checker = ctx.getBean(FileChecker.class);
+            checker.checkFileValidity();
+        }
+        public static void withSpringXmlConfig() {
+            System.out.println("🔥 Providing Dependencies With Xml Config 🔥");
+            ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+    
+            FileChecker checker = ctx.getBean(FileChecker.class);
+            checker.checkFileValidity();
+        }
+    
+        public static void main(String[] args) {
+            withSpring();
+            withSpringXmlConfig();
+        }
+    }
+}
