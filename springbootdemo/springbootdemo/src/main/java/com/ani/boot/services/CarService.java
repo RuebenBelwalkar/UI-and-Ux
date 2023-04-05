c175da028f1100032e6fbbc0ec298aadbac1f482
RuebenBelwalkar <ruebenbelwalkar10@gmail.com>
2023-04-05 12:37:09 +0530

    application properties

---
+++ springbootdemo/springbootdemo/src/main/java/com/ani/boot/services/CarService.java
@@ -0,0 +1,28 @@
+package com.ani.boot.services;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+
+import com.ani.boot.entity.Car;
+
+
+@PropertySource("myapp.yml")
+@Service
+public class CarService {
+
+    @Value("${testing.accounts.ws}")
+    private String website;
+
+    @Value("${testing.accounts.mail}")
+    private String mail;
+
+    public void createNewCar(Long id, String make, String model) {
+
+        
+        Car car = new Car();
+        car.setId(id);
+        car.setMake(make);
+        car.setModel(model);
+        System.out.println(car.toString());
+    }
+}
