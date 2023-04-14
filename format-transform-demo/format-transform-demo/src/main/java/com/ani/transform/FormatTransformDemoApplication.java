864f70b2e6a02e50a1d75306fbeda13845f9276e
RuebenBelwalkar <ruebenbelwalkar10@gmail.com>
2023-04-14 09:52:17 +0530

    transform json

--- format-transform-demo/format-transform-demo/src/main/java/com/ani/transform/FormatTransformDemoApplication.java
+++ format-transform-demo/format-transform-demo/src/main/java/com/ani/transform/FormatTransformDemoApplication.java
@@ -12,21 +12,19 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
-
 import com.ani.transform.domain.Author;
 import com.ani.transform.domain.Book;
 import com.ani.transform.domain.Publisher;
+import com.ani.transform.json.Config;
+import com.ani.transform.json.JsonDemo;
 import com.ani.transform.xml.Converter;
 
 @SpringBootApplication
 public class FormatTransformDemoApplication {
 
-	public static void main(String[] args) throws JAXBException, IOException {
-		ApplicationContext ctx = SpringApplication.run(FormatTransformDemoApplication.class, args);
-
+	public static void demo1(ApplicationContext ctx) throws JAXBException, IOException {
 
 		Converter converter = ctx.getBean(Converter.class);
-
 
 		Author author = new Author();
 		author.setFirstName("abc");
@@ -54,6 +52,15 @@ public class FormatTransformDemoApplication {
 		Resource res = new ClassPathResource("book.xml");
 		Book bk = converter.toDomain(res.getFile());
 		System.out.println(bk);
+	}
+
+	public static void main(String[] args) throws IOException  {
+		ApplicationContext ctx = SpringApplication.run(FormatTransformDemoApplication.class, args);
+
+		JsonDemo jd =  ctx.getBean(JsonDemo.class);
+		Config cfg = jd.toDomain(new ClassPathResource("demo.json").getFile().getAbsolutePath());
+		System.out.println(cfg);
+		System.out.println(jd.toJson(cfg));
 	}
 
 }
