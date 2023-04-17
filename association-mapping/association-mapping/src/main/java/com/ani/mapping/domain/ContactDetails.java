c4c027dea59546949c52dc97a4e36a5acd4548ab
RuebenBelwalkar <ruebenbelwalkar10@gmail.com>
2023-04-17 13:17:53 +0530

    one to one mapping

---
+++ association-mapping/association-mapping/src/main/java/com/ani/mapping/domain/ContactDetails.java
@@ -0,0 +1,16 @@
+package com.ani.mapping.domain;
+
+import javax.persistence.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+// @Setter
+// @Getter
+// @Entity
+public class ContactDetails {
+    private Long id;
+    private String mobile;
+    private String email;
+}
+
---
+++ association-mapping/association-mapping/src/main/java/com/ani/mapping/domain/Person.java
@@ -0,0 +1,26 @@
+package com.ani.mapping.domain;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long personId;
+    private String name;
+    private LocalDate dob;
+
+}
+
+
