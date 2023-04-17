813279846172c835e9bc5f3afe965efdad26cdb9
RuebenBelwalkar <ruebenbelwalkar10@gmail.com>
2023-04-17 13:17:26 +0530

    hibernate demo

---
+++ hibernate-setup/hibernate-setup/src/test/java/com/ani/orm/HibernateSetup1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ani.orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateSetup1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
