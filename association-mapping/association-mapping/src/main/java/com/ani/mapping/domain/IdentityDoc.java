b7ffee28c9fd5c051fe6290e2c2ccbf27c9ca97b
RuebenBelwalkar <ruebenbelwalkar10@gmail.com>
2023-04-17 15:56:33 +0530

    fetch lazy

--- association-mapping/association-mapping/src/main/java/com/ani/mapping/domain/IdentityDoc.java
+++ association-mapping/association-mapping/src/main/java/com/ani/mapping/domain/IdentityDoc.java
@@ -1,5 +1,6 @@
 package com.ani.mapping.domain;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,7 +10,9 @@ import javax.persistence.OneToOne;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
+@ToString
 @Setter
 @Getter
 @Entity
@@ -21,8 +24,26 @@ public class IdentityDoc {
     private String type;
     private Boolean isActive;
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL) //  if you delete/update/insert/ record from identity table, concerned record in Person table will also be deleted
     @JoinColumn(name = "person_id")
     private Person person;
 }
 
+// one identity can belongs to one person
+// one person can have one unique identity
+/*
+ 
+     create table identity_doc (
+       doc_id bigint not null,
+        is_active bit,
+        type varchar(255),
+        person_id bigint,
+        primary key (doc_id)
+    ) engine=InnoDB
+   
+    alter table identity_doc 
+       add constraint FKtniq3yhdrxcnc08xyg37knag5 
+       foreign key (person_id) 
+       references person (person_id)
+  
+ */
--- association-mapping/association-mapping/src/main/java/com/ani/mapping/domain/Person.java
+++ association-mapping/association-mapping/src/main/java/com/ani/mapping/domain/Person.java
@@ -1,9 +1,12 @@
 package com.ani.mapping.domain;
 
 
+
 import java.time.LocalDate;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -12,9 +15,11 @@ import javax.persistence.OneToOne;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
 
+@ToString
 @Setter
 @Getter
 @Entity
@@ -27,10 +32,10 @@ public class Person {
     private LocalDate dob;
 
     // bi-directional
-    @OneToOne(mappedBy = "person" )
+    @OneToOne(mappedBy = "person", fetch = FetchType.LAZY) // when select fired, do not select all records from identity
     private IdentityDoc identityDoc;
 
-    @OneToMany(mappedBy = "person")
+    @OneToMany(mappedBy = "person", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     private List<ContactDetails> contactDetails;
 
 }
@@ -48,4 +53,3 @@ public class Person {
         primary key (person_id)
     ) engine=InnoDB
 */
-
